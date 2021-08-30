3948edb704111465b53bcd80d4b0e49b40dacdf0
Sibints2000 <sibints2000@gmail.com>
2021-08-30 21:21:21 +0530

    Fifteenth Commit

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ NUTRI_APP_ID = "076ea41b"
 NUTRI_API_KEY = "c13eec2beeb2141474e9c037be8fa7da"
 
 exercise_endpoint = "https://trackapi.nutritionix.com/v2/natural/exercise"
+sheet_endpoint = "https://api.sheety.co"
 
 exercise_text = input("Tell me which exercises you did: ")
 
